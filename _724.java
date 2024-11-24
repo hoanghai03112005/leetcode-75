56be90d7caf2c422bcb51707492ab50f9235b533
hoanghai03112005 <hoanghai03112k5@gmail.com>
2024-11-24 14:18:39 +0700

    19/75

---
+++ _724.java
@@ -0,0 +1,26 @@
+package leetcode_75;
+
+public class _724 {
+	public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+        
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; 
+            }
+            
+
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
