0ea4c4ebbeb82251f31bf19f87c2cdbc3d7531cd
hoanghai03112005 <hoanghai03112k5@gmail.com>
2024-11-25 21:52:45 +0700

    23/ 75

--- _2352.java
+++ _2352.java
@@ -1,9 +1,45 @@
 package leetcode_75;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class _2352 {
 
-	public _2352() {
-		// TODO Auto-generated constructor stub
-	}
+	public int equalPairs(int[][] grid) {
+        Map<Integer, List<Integer>> row = new HashMap<Integer, List<Integer>>();
+        Map<Integer, List<Integer>> column = new HashMap<Integer, List<Integer>>();
+		
+		for (int i = 0; i < grid.length; i++) {
+			List<Integer> myList = new ArrayList<Integer>();
+			
+			for (int j = 0; j < grid.length; j++) {
+				myList.add(grid[i][j]);
+			}
+			row.put(i, myList);
+		}
+		
+		for (int i = 0; i < grid.length; i++) {
+			List<Integer> myList = new ArrayList<Integer>();
+			
+			for (int j = 0; j < grid.length; j++) {
+				myList.add(grid[j][i]);
+			}
+			column.put(i, myList);
+		}
+		
+		int cnt = 0;
+		
+		for (Integer key : row.keySet()) { 
+		    for (Integer key2 : column.keySet()) {
+				if (row.get(key).equals(column.get(key2))) {
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+    }
 
 }
